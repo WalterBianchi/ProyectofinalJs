86d5ae9812fbe9be38d996efc1547d316bdc4511
Walter Bianchi <walterbianchi17@gmail.com>
2024-09-18 20:54:45 -0300

    agregue el titulo

---
+++ main.js
@@ -0,0 +1,12 @@
+// selecciono el contenedor 
+const tituloConteiner = document.getElementById("Conteiner");
+
+// creacion del h1
+
+const h1 = document.createElement("h1");
+
+h1.textContent = "Bicicletas de Triatlon" ;
+
+tituloConteiner.appendChild(h1);
+
+
